8fbbcdb8044ae8aab7353918c77e5fdd69e12407
Oarkflow <info@oarkflow.com>
2024-08-14 20:58:43 +0545

    update: add application rule

--- request.go
+++ request.go
@@ -45,21 +45,36 @@ func (r *RuleRequest) Validate(data any, callbackFn ...CallbackFn) (any, error) 
 	return r.rule.Validate(data, callbackFn...)
 }
 
+func handleConditions(request *GroupRequest, getCondition func(string) *Filter) (conditions []Condition) {
+	for _, con := range request.Filters {
+		if con.FilterKey != "" && getCondition != nil {
+			fil := getCondition(con.FilterKey)
+			if fil != nil {
+				conditions = append(conditions, fil)
+			}
+		} else {
+			conditions = append(conditions, con)
+		}
+	}
+	return
+}
+
+func handleGroupRequest(request *GroupRequest, getCondition func(string) *Filter) *FilterGroup {
+	if request == nil {
+		return nil
+	}
+	conditions := handleConditions(request, getCondition)
+	if len(conditions) > 0 {
+		return NewFilterGroup(request.Operator, request.Reverse, conditions...)
+	}
+	return nil
+}
+
 func (applicationRule *ApplicationRule) BuildRuleFromRequest(getCondition func(string) *Filter) {
 	rule := NewRule()
 	if len(applicationRule.Rule.Conditions) > 0 {
 		for _, cons := range applicationRule.Rule.Conditions {
-			var conditions []Condition
-			for _, con := range cons.Filters {
-				if con.FilterKey != "" && getCondition != nil {
-					fil := getCondition(con.FilterKey)
-					if fil != nil {
-						conditions = append(conditions, fil)
-					}
-				} else {
-					conditions = append(conditions, con)
-				}
-			}
+			conditions := handleConditions(cons, getCondition)
 			if len(conditions) > 0 {
 				rule.AddCondition(cons.Operator, cons.Reverse, conditions...)
 			}
@@ -68,39 +83,13 @@ func (applicationRule *ApplicationRule) BuildRuleFromRequest(getCondition func(s
 	if len(applicationRule.Rule.Groups) > 0 {
 		var groups []Condition
 		for _, cons := range applicationRule.Rule.Groups {
-			var left []Condition
-			if cons.Left != nil {
-				for _, con := range cons.Left.Filters {
-					if con.FilterKey != "" && getCondition != nil {
-						fil := getCondition(con.FilterKey)
-						if fil != nil {
-							left = append(left, fil)
-						}
-					} else {
-						left = append(left, con)
-					}
-				}
-				if len(left) > 0 {
-					group := NewFilterGroup(cons.Left.Operator, cons.Left.Reverse, left...)
-					groups = append(groups, group)
-				}
+			left := handleGroupRequest(cons.Left, getCondition)
+			if left != nil {
+				groups = append(groups, left)
 			}
-			var right []Condition
-			if cons.Right != nil {
-				for _, con := range cons.Right.Filters {
-					if con.FilterKey != "" {
-						fil := getCondition(con.FilterKey)
-						if fil != nil {
-							right = append(right, fil)
-						}
-					} else {
-						right = append(right, con)
-					}
-				}
-				if len(right) > 0 {
-					group := NewFilterGroup(cons.Right.Operator, cons.Right.Reverse, right...)
-					groups = append(groups, group)
-				}
+			right := handleGroupRequest(cons.Right, getCondition)
+			if right != nil {
+				groups = append(groups, right)
 			}
 			if len(groups) > 0 {
 				rule.AddCondition(cons.Operator, cons.Reverse, groups...)
