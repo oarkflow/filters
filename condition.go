e1d569be1594e26b7a466490511e461bbc8f1536
Oarkflow <info@oarkflow.com>
2024-09-29 13:52:54 +0545

    bugfix: remove unnecessary conversion

--- condition.go
+++ condition.go
@@ -324,11 +324,7 @@ func stringOperation(data, value any, op func(string, string) bool) bool {
 }
 
 func checkIn(data, value any) bool {
-	sl, ok := convert.ToSlice(data, value)
-	if !ok {
-		return false
-	}
-	return utils.Contains(sl, data)
+	return utils.Contains(data, value)
 }
 
 func checkNotIn(data, value any) bool {
